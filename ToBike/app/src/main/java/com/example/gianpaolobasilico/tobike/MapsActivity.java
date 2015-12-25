658ca5538436df03ccb594823e27cd4b5b7129b8
Gianpaolo Basilicò <giapaolo.basilico@gmail.com>
2015-12-26 00:28:04 +0100

    item menu laterale, sistemata icona navigation bar menu, accenno alla localizzazione ogni tot secondi

--- ToBike/app/src/main/java/com/example/gianpaolobasilico/tobike/MapsActivity.java
+++ ToBike/app/src/main/java/com/example/gianpaolobasilico/tobike/MapsActivity.java
@@ -5,6 +5,7 @@ package com.example.gianpaolobasilico.tobike;
 // https://developers.google.com/maps/documentation/android-api/utility/marker-clustering?hl=en
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.location.Location;
@@ -15,13 +16,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +35,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -51,7 +56,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener{
 
     private GoogleMap mMap;
     private ProgressDialog pDialog;
@@ -71,6 +76,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Circle myLocationCircle;
     private CircleOptions myLocationCircleOptions;
     private String[] navigation_items;
+    private int[] icon_list;
+    private LocationRequest mLocationRequest;
 
 
     String url = "http://api.citybik.es/to-bike.json";
@@ -96,6 +103,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         myLocationCircleOptions.strokeColor(Color.BLUE);
         myLocationCircleOptions.strokeWidth(2);
 
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            //as soon as map is connected
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+
+
         //set floating action button;
         myLocation=(FloatingActionButton)findViewById(R.id.position);
         myLocation.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +129,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     myLocationCircleOptions.fillColor(Color.YELLOW);
                     myLocationCircleOptions.strokeColor(Color.BLUE);
                     myLocationCircleOptions.strokeWidth(2);
+                    if(myLocationCircle!=null)
+                        myLocationCircle.remove();
+                    myLocationCircleOptions.center(new LatLng(myLatitude, myLongitude));
                     myLocationCircle= mMap.addCircle(myLocationCircleOptions);
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLatitude, myLongitude),16));
                 }
@@ -119,24 +141,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         });
 
         TakeMeTo=(FloatingActionButton)findViewById(R.id.navigation);
-
-        //navigationDrawer
-        //navigationDrawerm
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        //get toolbar from xml and set it as actionbar
+        mDrawerList=(ListView)findViewById(R.id.drawer_items);
         toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_18dp);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        // Set the drawer toggle as the DrawerListener
         mDrawerLayout.setDrawerListener(drawerToggle);
-        navigation_items= new String[]{"Preferiti","Impostazioni","cazzabibbolo","chi siamo", "obladiObada"};
-        mDrawerList=(ListView)findViewById(R.id.drawer_items);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item,R.id.textViewList,navigation_items));
-        // Set the list's click listener
+
+        navigation_items= new String[]{getString(R.string.login),getString(R.string.setting),getString(R.string.preferred),
+                getString(R.string.whoweare)};
+        icon_list = new int[]{R.drawable.login,R.drawable.ic_settings_black_24dp,R.drawable.ic_add_location_black_24dp,
+                R.drawable.ic_person_black_24dp};
+
+        MAdapterList mAdapter=new MAdapterList(this, navigation_items,icon_list);
+        mDrawerList.setAdapter(mAdapter);
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_closed) {
@@ -153,20 +174,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
-
-
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-            //as soon as map is connected
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-        }
-
-
     }
 
     protected void onStart() {
@@ -178,6 +185,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mGoogleApiClient.disconnect();
         super.onStop();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected())
+            stopLocationUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+    }
+
+
+
 
 
     @Override
@@ -217,6 +242,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
+         drawerToggle.syncState();
     }
 
     @Override
@@ -270,18 +296,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
         setUpClusterer();
         mMap.setInfoWindowAdapter(mClusterManager.getMarkerManager());
-
         doReq();
-        LatLng torino = new LatLng(45.0585363,7.6882472);
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(torino));
-
     }
 
     public void doReq(){
         JsonArrayRequest req = new JsonArrayRequest(url,
                 new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONArray response) { Log.d("ciao",response.toString());
+                    public void onResponse(JSONArray response) {
                         try {
                             // Parsing json array response
                             // loop through each json object
@@ -326,7 +348,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
                 }, new Response.ErrorListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {   Log.d("ciao", error.toString());
+            public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(),
                         error.getMessage(), Toast.LENGTH_SHORT).show();
                 hidepDialog();
@@ -344,18 +366,40 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onConnected(Bundle bundle) {
         //as soon as map is connected
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation != null) {
             myLatitude=mLastLocation.getLatitude();
-            myLongitude=mLastLocation.getLongitude();}
-        myLocationCircleOptions.center(new LatLng(myLatitude, myLongitude));
+             myLongitude=mLastLocation.getLongitude();
+            if(myLocationCircle!=null)
+                myLocationCircle.remove();
+            myLocationCircleOptions.center(new LatLng(myLatitude, myLongitude));
+            myLocationCircle= mMap.addCircle(myLocationCircleOptions);
+             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLatitude, myLongitude),16));
+             createLocationRequest();
+             startLocationUpdates();}
+    }
 
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        updateUI();
+    }
+
+    private void updateUI() {
+        myLatitude=mLastLocation.getLatitude();
+        myLongitude=mLastLocation.getLongitude();
         if(myLocationCircle!=null)
-             myLocationCircle.remove();
+            myLocationCircle.remove();
+        myLocationCircleOptions.center(new LatLng(myLatitude, myLongitude));
         myLocationCircle= mMap.addCircle(myLocationCircleOptions);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLatitude, myLongitude),16));
-
     }
 
     @Override
@@ -369,11 +413,62 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(500);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
+
+    private class MAdapterList extends BaseAdapter {
+
+        private final Context context;
+        private final String[] navigation_items;
+        private final int[] item_icons;
+
+
+        public MAdapterList(Context context, String[] navigation_items, int[] item_icon) {
+            this.context = context;
+            this.navigation_items = navigation_items;
+            this.item_icons = item_icon;
+        }
+
+        @Override
+        public int getCount() {
+            return navigation_items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater= (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.drawer_list_item,parent,false);
+            TextView text = (TextView)rowView.findViewById(R.id.textViewItem);
+            ImageView icon=(ImageView)rowView.findViewById(R.id.icon_item);
+            text.setText(navigation_items[position]);
+            icon.setImageResource(item_icons[position]);
+            return rowView;
+        }
+    }
+
+
+     //handling  the navigation drawer's click
     private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
         }
     }
+
+
 }
